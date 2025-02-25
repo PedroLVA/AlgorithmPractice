bdd2389d8914ad4c29d90785f89ce4aba58c7efe
Pedro Athayde <pedro_athayde@outlook.com>
2025-02-25 11:53:41 -0300

    feat: Between Sets

--- src/Classes/HackerRank.java
+++ src/Classes/HackerRank.java
@@ -1,5 +1,54 @@
 package Classes;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
 public class HackerRank {
 
+    public static int getTotalX(List<Integer> a, List<Integer> b) {
+        // Write your code here
+        //There will be two arrays of integers. Determine all integers that satisfy the following two conditions:
+        //
+        //The elements of the first array are all factors of the integer being considered (numeros que dividem o numero exatamente)
+        //The integer being considered is a factor of all elements of the second array (Numeros que o numero divide exatamente)
+        //These numbers are referred to as being between the two arrays. Determine how many such numbers exist.
+
+        //The number we are looking for must be divisible by all numbers in the first array and divide all numbers in the second array
+
+
+        int maxA = Collections.max(a);
+        int minB = Collections.min(b);
+        HashSet<Integer> numsA = new HashSet<Integer>();
+        HashSet<Integer> numsB = new HashSet<Integer>();
+        int count = 0;
+        for(int i=maxA; i <= minB; i++){
+
+            int finalI = i;
+            if(a.stream().allMatch(num -> finalI % num == 0)){
+                numsA.add(finalI);
+            }
+
+        }
+        for(int i = 1; i <= minB; i++){
+           if(minB % i == 0){
+               numsB.add(i);
+           }
+        }
+        for(int num : numsA){
+            if(numsB.contains(num)){
+               count++;
+            }
+        }
+        System.out.println(numsA);
+        System.out.println(numsB);
+        return count;
+
+
+        //dentre 100, os numeros que são divididos sem resto pelo primeiro array e colocar eles no hashset
+
+        //
+
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import Classes.HackerRank;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        List<Integer> a = List.of(2);
+        List<Integer> b = List.of(20,30,12);
+
+        System.out.println(HackerRank.getTotalX(a,b));
     }
 }
