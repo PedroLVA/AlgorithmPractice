2b603fe343cd080a4fd7772300b291f2cb7c840c
Pedro Athayde <pedro_athayde@outlook.com>
2025-03-17 14:32:14 -0300

    feat: add angryProfessor.

--- src/Problems/HackerRank.java
+++ src/Problems/HackerRank.java
@@ -325,6 +325,22 @@ public class HackerRank {
         return treeHeight;
 
     }
+    public static String angryProfessor(int k, List<Integer> a) {
+        int count = 0;
+
+        for(int i = 0 ; i < a.size(); i++){
+            if(a.get(i) > 0){
+                count++;
+            }
+        }
+        if(a.size() - count >= k){
+            return "NO";
+        }
+        else{
+            return "YES";
+        }
+
+    }
 
 
 
