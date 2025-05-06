d067da09247faa638b6b954a343a940c84171c67
Pedro Athayde <pedro_athayde@outlook.com>
2025-05-06 11:54:48 -0300

    feat: leetcode longestConsecutiveHashSet try.

--- src/Problems/LeetCode.java
+++ src/Problems/LeetCode.java
@@ -3,6 +3,7 @@ package Problems;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 
 public class LeetCode {
 
@@ -13,17 +14,40 @@ public class LeetCode {
         Arrays.sort(nums);
         //sorted array
 
-        for(int i = 0; i < nums.length - 1; i++){
-            if(nums[i + 1] == nums[i] + 1 ){
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i + 1] == nums[i] + 1) {
                 count++;
-            }
-            else{
+            } else {
                 maxCount = Math.max(maxCount, count);
                 count = 1;
             }
         }
         maxCount = Math.max(maxCount, count);
 
+        return maxCount;
+
+    }
+
+    public static int longestConsecutiveHashSet(int[] nums) {
+        //this solution works but further optimizations needed for log(n)
+        HashSet<Integer> hashSet = new HashSet<>();
+        int maxCount = 0;
+
+        for (int num : nums) {
+            hashSet.add(num);
+        }
+
+        for (int num : hashSet) {
+            int currentNum = num;
+            int count = 1;
+
+            while (hashSet.contains(currentNum + 1)) {
+                currentNum++;
+                count++;
+            }
+
+            maxCount = Math.max(count, maxCount);
+        }
         return maxCount;
 
     }
