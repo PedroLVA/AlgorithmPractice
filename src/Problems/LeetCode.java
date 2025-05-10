eeff647f76679be0bfe994fa26b60c5b7b4a3ae2
Pedro Athayde <pedro_athayde@outlook.com>
2025-05-10 11:38:50 -0300

    feat: add hasCycle.

--- src/Problems/LeetCode.java
+++ src/Problems/LeetCode.java
@@ -5,6 +5,15 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+
 public class LeetCode {
 
     public static int longestConsecutive(int[] nums) {
@@ -54,6 +63,27 @@ public class LeetCode {
 
         }
         return maxCount;
+
+    }
+    public boolean hasCycle(ListNode head) {
+
+        HashSet<ListNode> visited = new HashSet<>();
+
+        ListNode current = head;
+
+        while (current != null) {
+
+            if (visited.contains(current)) {
+                return true;
+            }
+
+            visited.add(current);
+
+            current = current.next;
+
+        }
+
+        return false;
 
     }
 }
